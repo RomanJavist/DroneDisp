19f737a95df23cf30852a4234854e97e9553e56c
Stanislav Nemirovsky <choonsky@gmail.com>
2022-12-12 15:01:38 +0600

    Initial commit

---
+++ src/main/java/com/nemirovsky/dronedispatcher/DroneDispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.nemirovsky.dronedispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneDispatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nemirovsky/dronedispatcher/DroneDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemirovsky.dronedispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneDispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
