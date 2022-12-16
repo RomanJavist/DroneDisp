d86d5d31a3a7e710e5e3804fe94a5b99683a36eb
Stanislav Nemirovsky <choonsky@gmail.com>
2022-12-16 16:16:51 +0600

    Controller, tests, static html+css

---
+++ src/test/java/com/nemirovsky/dronedispatcher/DroneControllerTest.java
@@ -0,0 +1,60 @@
+package com.nemirovsky.dronedispatcher;
+
+import com.nemirovsky.dronedispatcher.controller.DroneController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@WebMvcTest(controllers = DroneController.class)
+public class DroneControllerTest {
+
+    @Value("${program.name}")
+    private String programName;
+
+    @Value("${program.version}")
+    private String programVersion;
+    @Autowired
+    private MockMvc mockMvc;
+    private final String helloString = programName + " " + programVersion;
+
+    @Test
+    public void getAllDrones() throws Exception {
+
+
+        ResultActions resultActions = mockMvc.perform(get("/drones"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("drones"))
+                .andExpect(model().attribute("helloMessage", equalTo(helloString)))
+                .andExpect(content().string(containsString(helloString)));
+
+        MvcResult mvcResult = resultActions.andReturn();
+        ModelAndView mv = mvcResult.getModelAndView();
+    }
+
+    @Test
+    public void getDroneById() throws Exception {
+        mockMvc.perform(get("/drone/D001"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("drones"))
+                .andExpect(model().attribute("helloMessage", equalTo(helloString)))
+                .andExpect(content().string(containsString(helloString)));
+    }
+
+
+}
